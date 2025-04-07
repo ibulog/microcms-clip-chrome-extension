7e99cc03d1fe78233cecb52e4fdc1b5cebb4ecf6
ibulog <ibulog76@gmail.com>
2025-04-07 17:57:52 +0900

    refactor: スタイルの記述を切り出し

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { createClient } from "microcms-js-sdk";
-import "./App.css";
+import "../../styles/App.css";
 
 import type { MicroCMSListContent } from "microcms-js-sdk";
 
